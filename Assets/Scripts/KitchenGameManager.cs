0333c15d64e70936e065d1504fe16b71efdd263b
BabySharpTuturu <166368972+BabySharpTuturu@users.noreply.github.com>
2024-06-28 22:43:36 +0300

    version without clock

--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class KitchenGameManager : MonoBehaviour {
+public class KitchenGameManager : MonoBehaviour
+{
 
 
     public static KitchenGameManager Instance { get; private set; }
@@ -15,7 +14,8 @@ public class KitchenGameManager : MonoBehaviour {
     public event EventHandler OnGameUnpaused;
 
 
-    private enum State {
+    private enum State
+    {
         WaitingToStart,
         CountdownToStart,
         GamePlaying,
@@ -30,79 +30,97 @@ public class KitchenGameManager : MonoBehaviour {
     private bool isGamePaused = false;
 
 
-    private void Awake() {
+    private void Awake()
+    {
         Instance = this;
 
         state = State.WaitingToStart;
     }
 
-    private void Start() {
+    private void Start()
+    {
         GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;
         GameInput.Instance.OnInteractAction += GameInput_OnInteractAction;
     }
 
-    private void GameInput_OnInteractAction(object sender, EventArgs e) {
-        if (state == State.WaitingToStart) {
+    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    {
+        if (state == State.WaitingToStart)
+        {
             state = State.CountdownToStart;
             OnStateChanged?.Invoke(this, EventArgs.Empty);
         }
     }
 
-    private void GameInput_OnPauseAction(object sender, EventArgs e) {
+    private void GameInput_OnPauseAction(object sender, EventArgs e)
+    {
         TogglePauseGame();
     }
 
-    private void Update() {
-        switch (state) {
+    private void Update()
+    {
+        switch (state)
+        {
             case State.WaitingToStart:
                 break;
             case State.CountdownToStart:
                 countdownToStartTimer -= Time.deltaTime;
-                if (countdownToStartTimer < 0f) {
+                if (countdownToStartTimer < 0f)
+                {
                     state = State.GamePlaying;
                     gamePlayingTimer = gamePlayingTimerMax;
                     OnStateChanged?.Invoke(this, EventArgs.Empty);
                 }
                 break;
             case State.GamePlaying:
-                gamePlayingTimer -= Time.deltaTime;
-                if (gamePlayingTimer < 0f) {
+                gamePlayingTimer += Time.deltaTime;// there was -
+                if (gamePlayingTimer < 0f)
+                {
                     state = State.GameOver;
                     OnStateChanged?.Invoke(this, EventArgs.Empty);
                 }
                 break;
-            case State.GameOver:
-                break;
+                //case State.GameOver:
+                //    break;
         }
     }
 
-    public bool IsGamePlaying() {
+    public bool IsGamePlaying()
+    {
         return state == State.GamePlaying;
     }
 
-    public bool IsCountdownToStartActive() {
+    public bool IsCountdownToStartActive()
+    {
         return state == State.CountdownToStart;
     }
 
-    public float GetCountdownToStartTimer() {
+    public float GetCountdownToStartTimer()
+    {
         return countdownToStartTimer;
     }
 
-    public bool IsGameOver() {
+    public bool IsGameOver()
+    {
         return state == State.GameOver;
     }
 
-    public float GetGamePlayingTimerNormalized() {
+    public float GetGamePlayingTimerNormalized()
+    {
         return 1 - (gamePlayingTimer / gamePlayingTimerMax);
     }
 
-    public void TogglePauseGame() {
+    public void TogglePauseGame()
+    {
         isGamePaused = !isGamePaused;
-        if (isGamePaused) {
+        if (isGamePaused)
+        {
             Time.timeScale = 0f;
 
             OnGamePaused?.Invoke(this, EventArgs.Empty);
-        } else {
+        }
+        else
+        {
             Time.timeScale = 1f;
 
             OnGameUnpaused?.Invoke(this, EventArgs.Empty);
