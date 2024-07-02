ff635f1ea9da0985e44b2888fe28c7974bf9da6f
Alesya Yarmosh <159628513+azzie24@users.noreply.github.com>
2024-07-02 10:26:59 +0300

    level selection done

--- Assets/Scripts/UI/InstructionUI.cs
+++ Assets/Scripts/UI/InstructionUI.cs
@@ -7,8 +7,7 @@ public class InstructionUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UpdateVisual();
-
+        
         Show();
     }
 
